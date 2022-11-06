cc38eababd81dcdca6808c9632145c63c6187ee3
lucasrpatten <lucasrpattendev@gmail.com>
2022-11-05 18:35:00 -0600

    threading, classes, argparse added

--- src/cmd_line_args.py
+++ src/cmd_line_args.py
@@ -1,117 +1,15 @@
 """Manages command line arguments"""
 
-from dataclasses import dataclass
-import sys
+import argparse
+from network_scanner import send_arp
 
 
-@dataclass
-class Flag:
-    """Represents a command line flag"""
-    name: str
-    help_message: str
-    exclusive: list[str] = ()
-    aliases: list[str] = ()
+parser = argparse.ArgumentParser(prog="ManInTheMirror")
+parser.add_argument("-o", "--option")
+args = parser.parse_args()
 
 
-@dataclass
-class Argument:
-    """Represents a command line argument"""
-    value: str | int
-
-
-class Flags:
-    """Contains all command line flags"""
-
-    def __init__(self) -> None:
-        self.help_flag = Flag(
-            name="help",
-            aliases=("h"),
-            exclusive=["*"],
-            help_message="Display this help menu"
-        )
-
-        self.other_flag = Flag(
-            name="other",
-            aliases=["o"],
-            help_message="pass",
-            exclusive=["help"]
-        )
-
-        self.flags = (self.help_flag, self.other_flag)
-
-
-class Arguments(Flags):
-    """Command line argument management"""
-
-    def __init__(self):
-        Flags.__init__(self)
-        args = sys.argv
-        self.args = args[1:]
-        self.flag_args = [i.lstrip('-') for i in self.args if i.startswith('-')]
-        self.command_names = self.get_cmd_names()
-
-    def get_cmd_names(self) -> list:
-        """Get the names of all the flags
-
-        Returns:
-            command_names: the names of the commands
-        """
-        command_names = []
-        for i in self.flag_args:
-            command_names.append(self.which_flag(i))
-        return command_names
-
-    def which_flag(self, arg: str):
-        """Determines which flag the argument is for
-
-        Returns:
-            flag_name: The name of the flag
-        """
-        for flag in self.flags:
-            if arg == str(flag.name) or arg in list[str](flag.aliases):
-                return str(flag.name)
-            else:
-                pass
-        raise Exception(f"{arg} is not a valid argument")
-
-    def exclusive_list(self, name: str) -> list[str]:
-        """
-            Determines what arguments are exclusive with given one
-        Returns:
-            exclusive_list: list of exclusive arguments
-        """
-        # remove use of eval if possible
-        return eval(f'self.{name}_flag.exclusive')
-
-    def check_collisions(self):
-        """raises exception if there are exclusive collisions"""
-        for name in self.command_names:
-            for exclusive in self.exclusive_list(name):
-                if exclusive in self.command_names:
-                    raise Exception(f"'{exclusive}' argument is exclusive to '{name}'")
-                elif exclusive == "*" and len(self.command_names) > 1:
-                    raise Exception(f"the '{name}' argument must be the only flag passed")
-
-    def help_command(self):
-        """Prints out the help menu"""
-        heading = f"|{'Command':^10}|{'Aliases':^20}|{'Exclusive To':^40}|{'Description':^60}|"
-        print(heading)
-        print('─' * (10 + 20 + 40 + 60 + 5))
-        for flag in self.flags:
-            name = '--' + flag.name
-            aliases = str(['-' + i for i in flag.aliases])
-            exclusives = str(flag.exclusive)
-            description = flag.help_message
-            print(f"|{name :^10}|{aliases:^20}|{exclusives:^40}|{description:^60}|")
-
-    def parse_cmd_line(self):
-        """
-        Parse the command line arguments to properly initiate the program.
-        """
-        self.check_collisions()
-        for flag in self.command_names:
-            match flag:
-                case 'help':
-                    self.help_command()
-                case _:
-                    pass
+if parser.option:
+    pass
+else:
+    send_arp()
--- src/network_scanner.py
+++ src/network_scanner.py
@@ -1,100 +1,139 @@
 """Scans the network"""
 
+import os
 import socket
 import struct
 import uuid
 import binascii
+import threading
 
 
-def get_mac_addr():
-    """get local mac address
-
-    Returns:
-        bytes: mac address
+class NetworkScanner:
+    """Class containing network scanning tools
     """
-    mac = uuid.UUID(int=uuid.getnode()).hex[-12:]  # mac as 48 bit int
-    return bytes.fromhex(mac)
+
+    def __init__(self):
+        self.get_mac_addr()
+        self.get_ip_addr()
+        self.run = True
+        self.net_list: list[tuple(str, str)] = []
+
+    def get_mac_addr(self):
+        """get local mac address
+
+        Returns:
+            bytes: mac address
+        """
+        mac = uuid.UUID(int=uuid.getnode()).hex[-12:]  # mac as 48 bit int
+        self.mac_addr = bytes.fromhex(mac)
+
+    def get_ip_addr(self):
+        """get local ip address
+
+        Returns:
+            _RetAddress: local ip
+        """
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # pylint: disable=fixme, invalid-name
+        # Use invalid/unused IP address
+        s.connect(('12.12.12.12', 6900))
+        local_ip = s.getsockname()[0]
+        s.close()
+        self.ip_addr = str(local_ip)
+
+    def recv_arp(self):
+        """ Scans the current network for arp responses
+        """
+        # sniff everything
+        sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(
+            0x0003))  # pylint: disable=fixme, invalid-name
+        while True:
+            if self.run is False:
+                break
+
+            packet = sock.recvfrom(2048)
+
+            ethernet_header = packet[0][0:14]
+            ethernet_detailed = struct.unpack("!6s6s2s", ethernet_header)
+
+            arp_header = packet[0][14:42]
+            arp_detailed = struct.unpack("2s2s1s1s2s6s4s6s4s", arp_header)
+
+            # skip non-ARP packets
+            ethertype = ethernet_detailed[2]
+            if ethertype != b'\x08\x06':
+                continue
+
+            ethertype = binascii.hexlify(ethertype)
+            hardware_type = binascii.hexlify(arp_detailed[0])  # pylint: disable=fixme, unused-variable
+            protocol_type = binascii.hexlify(arp_detailed[1])  # pylint: disable=fixme, unused-variable
+            hardware_size = binascii.hexlify(arp_detailed[2])  # pylint: disable=fixme, unused-variable
+            protocol_size = binascii.hexlify(arp_detailed[3])  # pylint: disable=fixme, unused-variable
+            opcode = binascii.hexlify(arp_detailed[4])  # pylint: disable=fixme, unused-variable
+            source_mac = binascii.hexlify(arp_detailed[5])
+            source_ip = socket.inet_ntoa(arp_detailed[6])
+            dest_mac = binascii.hexlify(arp_detailed[7])  # pylint: disable=fixme, unused-variable
+            dest_ip = socket.inet_ntoa(arp_detailed[8])  # pylint: disable=fixme, unused-variable
+            response = (source_ip, source_mac.decode())
+            if response not in self.net_list:
+                self.net_list.append(response)
+        sock.close()
+        self.run = True
+        return
+
+    def send_arp(self, ip_number):
+        """sends an arp request
+        """
+        interface = socket.if_nameindex()[1][1]
+
+        destination = b'\xff\xff\xff\xff\xff\xff'
+        split_ip = self.ip_addr.split('.')
+        dst_ip = '.'.join(i for i in split_ip[:-1]) + '.' + str(ip_number)
+        protocol = 0x0806  # 0x0806 is reserved for ARP
+
+        eth_header = struct.pack("!6s6sH", destination, self.mac_addr, protocol)
+
+        htype = 1  # Hardware type: Ethernet
+        ptype = 0x0800  # Protocol type: TCP IPV4
+        hlen = 6  # Hardware Address Length
+        plen = 4  # Protocl Address Length
+        operation = 1  # request operations
+        src = socket.inet_aton(self.ip_addr)
+        dst = socket.inet_aton(dst_ip)
+        arp_header = struct.pack("!HHBBH6s4s6s4s", htype, ptype,
+                                 hlen, plen, operation, self.mac_addr, src, destination, dst)
+
+        arp_packet = eth_header + arp_header
+
+        sock = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
+        sock.bind((interface, socket.htons(0x0800)))
+
+        sock.send(arp_packet)
+        sock.close()
+
+    def scan_with_arp(self):
+        """Scan all ports with arp"""
+        for i in range(0, 256):
+            self.send_arp(i)
+
+    def scan(self):
+        """ Scan the local network
+        """
+        arp_receiver = threading.Thread(target=self.recv_arp)
+        arp_scanner = threading.Thread(target=self.scan_with_arp)
+        arp_receiver.start()
+        arp_scanner.start()
+        arp_scanner.join()
+        self.run = False
+        arp_receiver.join()
+
+    def menu(self):
+        """Display the menu to choose who to perform attack on
+        """
+        os.system('clear')
+        for i in self.net_list:
+            print(i)
 
 
-def get_ip_addr():
-    """get local ip address
+ns = NetworkScanner()
 
-    Returns:
-        _RetAddress: local ip
-    """
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # pylint: disable=fixme, invalid-name
-    # Use invalid/unused IP address
-    s.connect(('12.12.12.12', 6900))
-    local_ip = s.getsockname()[0]
-    s.close()
-    return local_ip
-
-
-def recv_arp(source):
-    """ Scans the current network for arp responses
-    """
-    # sniff everything
-    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))  # pylint: disable=fixme, invalid-name
-    while True:
-
-        packet = s.recvfrom(2048)
-
-        ethernet_header = packet[0][0:14]
-        ethernet_detailed = struct.unpack("!6s6s2s", ethernet_header)
-
-        arp_header = packet[0][14:42]
-        arp_detailed = struct.unpack("2s2s1s1s2s6s4s6s4s", arp_header)
-
-        # skip non-ARP packets
-        ethertype = ethernet_detailed[2]
-        if ethertype != b'\x08\x06':
-            continue
-
-        ethertype = binascii.hexlify(ethertype)
-        hardware_type = binascii.hexlify(arp_detailed[0]) # pylint: disable=fixme, unused-variable
-        protocol_type = binascii.hexlify(arp_detailed[1]) # pylint: disable=fixme, unused-variable
-        hardware_size = binascii.hexlify(arp_detailed[2]) # pylint: disable=fixme, unused-variable
-        protocol_size = binascii.hexlify(arp_detailed[3]) # pylint: disable=fixme, unused-variable
-        opcode = binascii.hexlify(arp_detailed[4]) # pylint: disable=fixme, unused-variable
-        source_mac = binascii.hexlify(arp_detailed[5])
-        source_ip = socket.inet_ntoa(arp_detailed[6])
-        dest_mac = binascii.hexlify(arp_detailed[7]) # pylint: disable=fixme, unused-variable
-        dest_ip = socket.inet_ntoa(arp_detailed[8])
-        print(dest_ip, " ", get_mac_addr())
-        if dest_ip == get_ip_addr() and source_ip == source:
-            return (source_mac, source_ip)
-
-
-def send_arp():
-    """sends an arp request
-    """
-    interface = socket.if_nameindex()[1][1]
-    local_ip = get_ip_addr()
-
-    mac_addr = get_mac_addr()
-    destination = b'\xff\xff\xff\xff\xff\xff'
-    dst_ip = open('./.env', 'r', encoding='utf-8').read()
-    protocol = 0x0806  # 0x0806 is reserved for ARP
-
-    eth_header = struct.pack("!6s6sH", destination, mac_addr, protocol)
-
-    htype = 1  # Hardware type: Ethernet
-    ptype = 0x0800  # Protocol type: TCP IPV4
-    hlen = 6  # Hardware Address Length
-    plen = 4  # Protocl Address Length
-    operation = 1  # request operations
-    src = socket.inet_aton(local_ip)
-    dst = socket.inet_aton(dst_ip)
-    arp_header = struct.pack("!HHBBH6s4s6s4s", htype, ptype,
-                             hlen, plen, operation, mac_addr, src, destination, dst)
-
-    arp_packet = eth_header + arp_header
-
-    s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))  # pylint: disable=fixme, invalid-name
-    s.bind((interface, socket.htons(0x0800)))
-
-    s.send(arp_packet)
-    recv_arp(dst_ip)
-    s.close()
-
-send_arp()
+ns.scan()
