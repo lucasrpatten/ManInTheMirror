f2ded279232555cf930f12259f6ccdfb637e80ce
lucasrpatten <lucasrpattendev@gmail.com>
2023-05-13 17:37:17 -0600

    traffic management

--- src/traffic_manager.py
+++ src/traffic_manager.py
@@ -1,11 +1,39 @@
 import socket
+import time
 
 
-def capture_traffic(gateway_mac, gateway_ip, target_mac, target_ip, log_path="log.txt"):
+def capture_traffic(gateway_ip, gateway_mac, target_ip, target_mac, interface=socket.if_nameindex()[1][1], log_path="log.txt"):
     sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,
                          socket.ntohs(0x0003))
+
     sock.settimeout(1)
 
     with open(log_path, "a") as f:
+
         while True:
-            try
+            try:
+                packet, _ = sock.recvfrom(65535)
+                ip_header = packet[14:34]
+                source_ip = socket.inet_ntoa(ip_header[12:16])
+                dest_ip = socket.inet_ntoa(ip_header[16:20])
+
+                if source_ip == target_ip and dest_ip == gateway_ip:
+                    f.write(
+                        f"""\
+Time: {time.time()}
+Source: {source_ip}
+Destination: {dest_ip}
+Packet: {packet.hex()}
+
+""")
+                elif source_ip == gateway_ip and dest_ip == target_ip:
+                    f.write(
+                        f"""\
+Time: {time.time()}
+Source: {source_ip}
+Destination: {dest_ip}
+Packet: {packet.hex()}
+
+""")
+            except socket.timeout:
+                continue
